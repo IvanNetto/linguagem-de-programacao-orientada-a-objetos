5fa1b35fedd8030b2d3bb2e7a6873cedd5068426
Diego Moura <diegosouza20049@hotmail.com>
2020-10-15 06:53:20 -0300

    exemplos Listas

--- exemplos-praticos/src/Listas.java
+++ exemplos-praticos/src/Listas.java
@@ -11,6 +11,19 @@ public class Listas {
             System.out.println(fruta);
         }
 
+        //RETORNA A POSIÇÃO DO ITEM BANANA NA LISTA
+        lista.indexOf("banana");
+        //RETORNA O TAMANHO DA LISTA
+        lista.size();
+        //RETORNA SE A LISTA ESTÁ VAZIA
+        lista.isEmpty();
+        //ATUALIZA A POSIÇÃO 2 COM O ITEM PERA
+        lista.set(2, "pera");
+        //REMOVE O ITEM NA POSIÇÃO 0
+        lista.remove(0);
+        //REMOVE O ITEM BANANA
+        lista.remove("banana");
+
 
     }
 }
